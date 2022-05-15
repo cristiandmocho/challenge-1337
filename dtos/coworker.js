9e2af328a77636385813c939bfc371a35273916b
Cristian Mocho <cristiandmocho@gmail.com>
2022-05-15 09:46:20 +0100

    Preparing endpoint and DTO to use pagination and filter

--- dtos/coworker.js
+++ dtos/coworker.js
@@ -4,7 +4,7 @@ export async function getCoworkersCount() {
   const conn = await getDBConnection();
 
   try {
-    const [rows, _fields] = await conn.execute('select * from coworkers');
+    const [rows, _fields] = await conn.execute('select count(id) as rowCount from coworkers');
     return rows;
   } catch (err) {
     console.log(err);
@@ -14,11 +14,15 @@ export async function getCoworkersCount() {
   }
 }
 
-export async function getCoworkersList() {
+export async function getCoworkersList(start = 0, end = 0) {
   const conn = await getDBConnection();
+  const pageSize = Number(end) - Number(start);
+
+  let query = 'select * from coworkers';
+  if (pageSize > 0) query += ' limit ?, ?';
 
   try {
-    const [rows, _fields] = await conn.execute('select * from coworkers');
+    const [rows, _fields] = await conn.execute(query, [pageSize]);
     return rows;
   } catch (err) {
     console.log(err);
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { getCoworkerById, getCoworkerByName, getCoworkersCount, getCoworkersList } from '../dtos/coworker.js';
 import { Authenticate, ValidateToken } from '../middleware/auth.js';
 
 const router = Router();
@@ -9,7 +10,33 @@ router.post('/login', Authenticate, (_req, res) => {
 });
 
 // private endpoints
-router.get('/coworkers', ValidateToken, (req, res) => {});
-router.get('/coworker/:id', ValidateToken, (req, res) => {});
+router.get('/coworkers', ValidateToken, async (req, res) => {
+  const { start, end, filter } = req.params;
+  let data;
+
+  const rowCount = await getCoworkersCount();
+
+  if (!start && !end && !filter) data = await getCoworkersList();
+  if (start && end) data = await getCoworkersList(start, end);
+  if (filter) data = await getCoworkerByName(filter);
+
+  res.json({ data, totalLength: rowCount });
+});
+
+router.get('/coworker/:id', ValidateToken, async (req, res) => {
+  const { id } = req.params;
+  const data = await getCoworkerById(id);
+
+  res.json(data);
+});
+
+const updateHandler = (req, res) => {};
+
+// Recommended verb for partial updates (PATCH)
+router.patch('/coworker', ValidateToken, updateHandler);
+
+// Note: POST is not the recommended verb to do UPDATE operations!
+// This endpoint should be either PUT or PATCH
+router.post('/coworker', ValidateToken, updateHandler);
 
 export default router;
